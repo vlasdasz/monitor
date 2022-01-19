1feabdfbe0b17b0f44dc6b495f013659c3ece494
Vladas Zakrevskis <146100@gmail.com>
2022-01-19 19:41:45 +0200

    mb memory

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,24 @@ async fn main() {
     loop {
 
         println!("os: {} {}", os_type().unwrap(), os_release().unwrap());
+
         println!("cpu: {} cores, {} MHz", cpu_num().unwrap(), cpu_speed().unwrap());
+
         println!("proc total: {}", proc_total().unwrap());
+
         let load = loadavg().unwrap();
+
         println!("load: {} {} {}", load.one, load.five, load.fifteen);
+
         let mem = mem_info().unwrap();
-        println!("mem: total {} KB, free {} KB, avail {} KB, buffers {} KB, cached {} KB",
-                 mem.total, mem.free, mem.avail, mem.buffers, mem.cached);
-        println!("swap: total {} KB, free {} KB", mem.swap_total, mem.swap_free);
+
+        println!("mem: total {} MB, free {} MB, avail {} MB, buffers {} MB, cached {} MB",
+                 mem.total / 1024, mem.free / 1024, mem.avail / 1024, mem.buffers / 1024, mem.cached / 1024 );
+
         #[cfg(not(target_os = "solaris"))] {
             let disk = disk_info().unwrap();
             println!("disk: total {} KB, free {} KB", disk.total, disk.free);
         }
-        println!("hostname: {}", hostname().unwrap());
-        #[cfg(not(target_os = "windows"))] {
-            let t = boottime().unwrap();
-            println!("boottime {} sec, {} usec", t.tv_sec, t.tv_usec);
-        }
-        #[cfg(target_os = "linux")]
-        println!("/etc/os-release: {:?}", linux_os_release().unwrap());
 
         sleep(Duration::from_millis(5000)).await;
     }
