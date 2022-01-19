e2e407baa44311f639d20f288777a25ed47fa678
Vladas Zakrevskis <146100@gmail.com>
2022-01-19 19:49:46 +0200

    another crate

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,22 @@
 use tokio::time::{sleep, Duration};
-extern crate sys_info;
 
-use sys_info::*;
+use sysinfo::{NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
 
 
 #[tokio::main]
 async fn main() {
 
+    let mut sys = System::new_all();
+
     loop {
 
-        println!("os: {} {}", os_type().unwrap(), os_release().unwrap());
+        // First we update all information of our `System` struct.
+        sys.refresh_all();
 
-        println!("cpu: {} cores, {} MHz", cpu_num().unwrap(), cpu_speed().unwrap());
-
-        println!("proc total: {}", proc_total().unwrap());
-
-        let load = loadavg().unwrap();
-
-        println!("load: {} {} {}", load.one, load.five, load.fifteen);
-
-        let mem = mem_info().unwrap();
-
-        println!("mem: total {} MB, free {} MB, avail {} MB, buffers {} MB, cached {} MB",
-                 mem.total / 1024, mem.free / 1024, mem.avail / 1024, mem.buffers / 1024, mem.cached / 1024 );
-
-        #[cfg(not(target_os = "solaris"))] {
-            let disk = disk_info().unwrap();
-            println!("disk: total {} MB, free {} MB", disk.total / 1024, disk.free / 1024);
-        }
+        println!("total memory: {} KB", sys.total_memory());
+        println!("used memory : {} KB", sys.used_memory());
+        println!("total swap  : {} KB", sys.total_swap());
+        println!("used swap   : {} KB", sys.used_swap());
 
         sleep(Duration::from_millis(5000)).await;
     }
